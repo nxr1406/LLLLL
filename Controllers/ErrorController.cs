fd27c6609f56e8f6db406e06de66afcf8e3c6cc2
nxr1406 <chocqn2580@gmail.com>
2026-07-06 10:41:42 +0600

    update ErrorController.cs

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -4,10 +4,9 @@ namespace Portfolio.Controllers;
 
 public class ErrorController : Controller
 {
-    [Route("Error/NotFound")]
     public IActionResult PageNotFound()
     {
         Response.StatusCode = 404;
-        return View();
+        return View("NotFound");
     }
 }
